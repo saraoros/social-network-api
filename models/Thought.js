e29e494bbd4ba6bcb9384ac918d9f6f89f594f10
Sara Oros Gochez <saraorosgochez@Saras-MacBook-Air.local>
2022-04-25 16:19:11 -0700

    completed Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,22 +1,65 @@
-// Thought
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
-// thoughtText
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      length: [1, 280],
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
 
-// String
-// Required
-// Must be between 1 and 280 characters
-// createdAt
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      length: [1, 280],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [ReactionSchema],
+  },
 
-// Date
-// Set default value to the current timestamp
-// Use a getter method to format the timestamp on query
-// username (The user that created this thought)
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+  }
+);
 
-// String
-// Required
-// reactions (These are like replies)
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
 
-// Array of nested documents created with the reactionSchema
-// Schema Settings
+const Thought = model("Thought", ThoughtSchema);
 
-// Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+module.exports = Thought;
